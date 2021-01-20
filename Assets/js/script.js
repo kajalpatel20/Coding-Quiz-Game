3d9b80783cc8d9ebf2f21e2420822f35de5070ec
Kajal Patel <kajalpatel20@gmail.com>
2021-01-19 21:29:11 -0500

    add index.html, fixed timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,7 @@ var highScoreEl = document.getElementById("high-scores")
 var welcomeEl = document.getElementById("welcome");
 var startQuiz = document.getElementById("start-quiz");
 
-var timer = document.getElementById("timer-count");
+var timerEl = document.getElementById("timer-count");
 
 var questionEl = document.getElementById("questions")
 var questionTitle = document.getElementById("question-title");
@@ -19,6 +19,8 @@ var finalScore = document.getElementById("final-score");
 var playerInitials = document.getElementById("player-initials");
 var saveScoreBtn = document.getElementById("submit-score");
 
+var homeBtn = document.getElementById("go-back-btn");
+
 var leaderBoardEl = document.getElementById("board");
 var gameOverScreen = document.getElementById(".gameOver");
 
@@ -28,17 +30,17 @@ qIndex = 0;
 userScore = 0;
 
 
-//Set countdown timer to 30 seconds, start countdown function
-var timeLeft = 30;
+//Set countdown timer to 90 seconds, start countdown function
+var timeLeft = 90;
+var time;
 function updateTimer() {
-     timerInterval = setInterval(function () {
+     time = setInterval(function () {
 //when timer reaches zero, clear interval function and display game over
+        timeLeft--;
+        timerEl.textContent = timeLeft + " seconds left";
         if (timeLeft === 0) {
-               clearInterval(timerInterval);
-               gameOver();
-          } else {
-          timeLeft--;
-          timerDisplay.textContent = timeLeft + " seconds left";
+               clearInterval(time);
+               displayFinalScore();
           }
      }, 1000);
 }
@@ -49,39 +51,39 @@ function updateTimer() {
 const questions = [
     
     {
-        q: "Which of the following functions of String object returns the character at the specified index?",
-        c: ["substri()","indexOf()","charAt()","forEach()"],
-        a: 2 //"charAt()"
+        question: "Which of the following functions of String object returns the character at the specified index?",
+        choices: ["substri()","indexOf()","charAt()","forEach()"],
+        answer: 2 //"charAt()"
     },
     {
-        q: "What is the HTML tag under which one can write the JavaScript code?",
-        c: ["<javascript>","<scripted>","<script>","<js>"],
-        a: 2 //<script>
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        choices: ["<javascript>","<scripted>","<script>","<js>"],
+        answer: 2 //<script>
     },
     {
-        q: "How do you write a ‘logical or’ in Javascript code?",
-        c: ["//","$$","%","||",],
-        a: 3 //"||" 
+        question: "How do you write a ‘logical or’ in Javascript code?",
+        choices: ["//","$$","%","||",],
+        answer: 3 //"||" 
     },
     {
-        q: "Which of the following is not a reserved word in JavaScript?",
-        c: ["interface","throws","program","short"],
-        a: 2 //program
+        question: "Which of the following is not a reserved word in JavaScript?",
+        choices: ["interface","throws","program","short"],
+        answer: 2 //program
     },
     {
-        q: "What is the original name of JavaScript?",
-        c: ["LiveScript","EScript","Mocha","JavaScript"],
-        a: 2 //Mocha
+        question: "What is the original name of JavaScript?",
+        choices: ["LiveScript","EScript","Mocha","JavaScript"],
+        answer: 2 //Mocha
     },
     {
-        q: "What are the types of Pop up boxes available in JavaScript?",
-        c: ["Alert","Prompt","Confirm","All of the above"],
-        a: 3 //All of the above
+        question: "What are the types of Pop up boxes available in JavaScript?",
+        choices: ["Alert","Prompt","Confirm","All of the above"],
+        answer: 3 //All of the above
     },
     {
-        q: "The _______ method of an Array object adds and/or removes elements from an array.",
-        c: ["Reverse","Shift","Slice","Splice"],
-        a: 3 //Splice
+        question: "The _______ method of an Array object adds and/or removes elements from an array.",
+        choices: ["Reverse","Shift","Slice","Splice"],
+        answer: 3 //Splice
     }
 ]
 
@@ -107,7 +109,9 @@ choice4.addEventListener("click", function(){
 });
 saveScoreBtn.addEventListener("click",saveScoretoStorage);
 
-
+homeBtn.addEventListener("click", function(){
+    window.location.reload()
+})
 
 // main functions
 
@@ -125,8 +129,8 @@ function welcome(){
 
     var t = document.getElementById("welcome-title");
     var tx = document.getElementById("welcome-text");
-    t.textContent = "Javascript Coding Challenge 2";
-    tx.textContent = "get ready";
+    t.textContent = "Coding-Quiz-Game";
+    tx.textContent = "Get Ready";
     startQuiz.textContent = "Start Quiz";
 }
 
@@ -139,31 +143,39 @@ function initiateQuiz(){
     lineBreak.style.display = "none";
     resultCheck.style.display = "none";
     startquestions();
+    updateTimer();
 }
 
 function startquestions(){
-    questionTitle.textContent = questions[qIndex].q;
-    choice1.textContent = questions[qIndex].c[0];
-    choice2.textContent = questions[qIndex].c[1];
-    choice3.textContent = questions[qIndex].c[2];
-    choice4.textContent = questions[qIndex].c[3];
+    questionTitle.textContent = questions[qIndex].question;
+    choice1.textContent = questions[qIndex].choices[0];
+    choice2.textContent = questions[qIndex].choices[1];
+    choice3.textContent = questions[qIndex].choices[2];
+    choice4.textContent = questions[qIndex].choices[3];
 }
 
 // check answer function here
 function checkAnswer(pick){
     lineBreak.style.display = "block";
     resultCheck.style.display = "block";
-    if (pick === questions[qIndex].a){
+    if (pick === questions[qIndex].answer){
         resultCheck.textContent = "Correct!";         
         userScore++;
     } else {
-    resultCheck.textContent = "Wrong!";    
+
+        timeLeft -= 15;
+
+        if(timeLeft < 0){
+            timeLeft = 0;
+        }
+        resultCheck.textContent = "Wrong!";    
     }
     qIndex++;
     //console.log(qIndex);
     if (qIndex < questions.length){
         startquestions();
     } else {
+        clearInterval(time)
         displayFinalScore();
     }
 }
@@ -188,7 +200,7 @@ function saveScoretoStorage(){
         // retrieve it (Or create a blank array if there isn't any info saved yet),
         var savedScores = JSON.parse(localStorage.getItem('storedScores')) || [];
         // add to it,
-        savedScores.push({playerInitals: playerInitials, userScore: userScore});
+        savedScores.push({playerInitals: playerInitials.value, userScore: userScore});
         // then put it back.
         localStorage.setItem('storedScores', JSON.stringify(savedScores));
     //}
